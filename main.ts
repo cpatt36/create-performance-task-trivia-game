287027b0a5fed31a44d6b43f5048180e78f34a8c
cpatt36 <91620056+cpatt36@users.noreply.github.com>
2022-02-01 11:06:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+game.showLongText("Welcome to the \"Trivia Game\" hosted by Sara and Caroline!", DialogLayout.Full)
+forever(function () {
+    if (info.score() >= 5) {
+        game.over(true, effects.confetti)
+    }
+})
